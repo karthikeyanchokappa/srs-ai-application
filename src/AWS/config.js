a79e6423279ffedf8a6e8c811555c9850f9a32bd
karthikeyanchokappa <karthikeyan.chokappa@outlook.com>
2025-12-18 00:26:59 +1100

    reset

--- src/AWS/config.js
+++
@@ -1,5 +0,0 @@
-export const AWS_CONFIG = {
-  region: import.meta.env.VITE_AWS_REGION,
-  userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-  clientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
-};
